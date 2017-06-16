11ac913445c3b6b04162b8dce047c8c443197353
singhashish19031998@gmail.com <singhashish19031998@gmail.com>
2017-06-17 00:21:16 +0530

    Pin crud functionality added

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,10 @@
 class PinsController < ApplicationController
   def index
+    @pin = Pin.all
   end
-
+def show
+@pin = Pin.find(params[:id])
+end
   def new
     @pin = Pin.new
   end
@@ -9,19 +12,27 @@ class PinsController < ApplicationController
   def create
     @pin = Pin.new(pin_params)
     if @pin.save
-      redirect_to root_path
+      redirect_to root_path , notice: "successfully created new pin"
+    else render 'new'
     end
   end
 
 
   def edit
-  @pin = Pin.find(parms[:id])
+  @pin = Pin.find(params[:id])
   end
 
   def update
-   @pin = Pin.find(pin_params)
+   @pin = Pin.find(params[:id])
     if @pin.update(pin_params)
       redirect_to root_path
+    end
+  end
+
+  def destroy
+    @pin = Pin.find(params[:id])
+    if @pin.destroy
+      redirect_to root_path , notice: " pin is successfully deleted "
     end
   end
 
