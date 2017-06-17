808eb3c7ff58afa9a768386a2b0bbaeb4894b077
singhashish19031998@gmail.com <singhashish19031998@gmail.com>
2017-06-17 17:12:40 +0530

    DRY code applied for the first time

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,9 +1,13 @@
 class PinsController < ApplicationController
+  before_action :find_pin, only: [:show,:edit,:update,:destroy]
+
+
+
   def index
     @pin = Pin.all
   end
 def show
-@pin = Pin.find(params[:id])
+
 end
   def new
     @pin = Pin.new
@@ -19,24 +23,28 @@ end
 
 
   def edit
-  @pin = Pin.find(params[:id])
+
   end
 
   def update
-   @pin = Pin.find(params[:id])
+
     if @pin.update(pin_params)
       redirect_to root_path
     end
   end
 
   def destroy
-    @pin = Pin.find(params[:id])
+
     if @pin.destroy
       redirect_to root_path , notice: " pin is successfully deleted "
     end
   end
 
   private
+
+  def find_pin
+    @pin = Pin.find(params[:id])
+  end
 
  def pin_params
    params.require(:pin).permit(:title, :description)
