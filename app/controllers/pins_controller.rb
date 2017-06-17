650171389ba1c3a44240f4ed57bf849fe36673ed
singhashish19031998@gmail.com <singhashish19031998@gmail.com>
2017-06-17 18:15:19 +0530

    Some authorization is added

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,21 +1,22 @@
 class PinsController < ApplicationController
   before_action :find_pin, only: [:show,:edit,:update,:destroy]
-
-
-
+  before_action :current_user, only: [:create,:update, :destroy]
   def index
     @pin = Pin.all
   end
 def show
 
 end
+
   def new
     @pin = Pin.new
+      render 'new'
   end
 
   def create
     @pin = Pin.new(pin_params)
-    if @pin.save
+    @user = current_user
+    if @pin.save && @user.save
       redirect_to root_path , notice: "successfully created new pin"
     else render 'new'
     end
@@ -35,7 +36,7 @@ end
 
   def destroy
 
-    if @pin.destroy
+    if @pin.destroy(pin_params)
       redirect_to root_path , notice: " pin is successfully deleted "
     end
   end
@@ -46,9 +47,16 @@ end
     @pin = Pin.find(params[:id])
   end
 
+
+    def current_user
+      @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+
+
  def pin_params
    params.require(:pin).permit(:title, :description)
  end
+
 
 
 end
